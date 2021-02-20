b1dd7ded786b1b3a65603e2e85c7dd0413af9620
kianoush <kianoushit1999@gmail.com>
2021-02-20 13:22:02 -0800

    compeliting informations and adding comments components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsisV, faHeart as Heart } from '@fortawesome/free-solid-svg-icons';
-import {faHeart, faPaperPlane, faComment} from '@fortawesome/free-regular-svg-icons'
 import Faker from 'faker';
 import Post from './Post'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,27 +6,37 @@ import './App.css'
 
 let getRandom = () => Math.floor(Math.random() * 10) + 1
 
-let getRandomUsers = () =>{
-    let users;
+let getRandomComments = () => {
+    let comments = []
+    for (let i = 0; i < getRandom(); i++) {
+        comments.push({
+            name: Faker.internet.userName(),
+            commentContent: Faker.lorem.lines(2)
+        })
+    }
+    return comments;
+}
+
+let getRandomUsers = () => {
+    let users = [];
     for (let i = 0; i < getRandom(); i++) {
         users[i] = {
             name: Faker.internet.userName(),
             image: Faker.image.image(),
             postContent: Faker.lorem.lines(4),
-            comments:{
-                name: Faker.internet.userName(),
-                commentContent: Faker.lorem.lines(2)
-            }
+            comments: getRandomComments()
         }
     }
     return users;
 }
 
 const App = (props) => {
+    console.log(getRandomUsers())
     return (
         <div className="App">
             {getRandomUsers().map(user => {
-                <Post name={user.name} image={user.image} postContent={user.postContent} comments={user.comments}></Post>
+                return <Post name={user.name} image={user.image} postContent={user.postContent}
+                             comments={user.comments}></Post>
             })}
         </div>
     )
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,54 +2,64 @@ import React from "react";
 import {Card, Col, Container, Row} from "react-bootstrap";
 import Avatar from "react-avatar";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEllipsisV, faHeart as Heart} from "@fortawesome/free-solid-svg-icons";
-import Faker from "faker";
+import {faEllipsisV} from "@fortawesome/free-solid-svg-icons";
 import {faComment, faHeart, faPaperPlane} from "@fortawesome/free-regular-svg-icons";
 import Comment from "./Comment";
 
 
-
 const Post = (props) => {
-    let username = Faker.internet.userName();
+
+    let username = props.name;
+    let image = props.image;
+    let content = props.postContent;
+    let comments = props.comments;
 
     return (
-      <div>
-          <Container>
-              <Row>
-                  <Col xs={12} className="d-flex justify-content-center mx-auto mt-4">
-                      <Card className="card-centerize">
-                          <Card.Title>
-                              <Row>
-                                  <Col xs={11} className="d-flex justify-content-start">
-                                      <Avatar name={username} size="25" textSizeRatio={1} round={true}/>
-                                      <div className="ml-1">
-                                          {username}
-                                      </div>
-                                  </Col>
-                                  <Col xs={1}>
-                                      <FontAwesomeIcon icon={faEllipsisV}></FontAwesomeIcon>
-                                  </Col>
-                              </Row>
-                          </Card.Title>
-                          <Card.Img variant="top" src={Faker.image.image()} className="fix-pictures"/>
-                          <Card.Body className="px-0 pt-1">
-                              <div>
-                                  <span className="px-1"><FontAwesomeIcon icon={faHeart} /></span>
-                                  <span className="px-1"><FontAwesomeIcon icon={faPaperPlane} /></span>
-                                  <span className="px-1"><FontAwesomeIcon icon={faComment} /></span>
-                              </div>
-                              <Card.Text>
-                                  {Faker.lorem.lines(4)}
-                                  <br />
-                                  <small style={{color: 'grey'}}>5h</small>
-                              </Card.Text>
-                              <Comment></Comment>
-                          </Card.Body>
-                      </Card>
-                  </Col>
-              </Row>
-          </Container>
-      </div>
+        <div>
+            <Container>
+                <Row>
+                    <Col xs={12} className="d-flex justify-content-center mx-auto mt-4">
+                        <Card className="card-centerize">
+                            <Card.Title>
+                                <Row>
+                                    <Col xs={11} className="d-flex justify-content-start">
+                                        <Avatar name={username} size="25" textSizeRatio={1} round={true}/>
+                                        <div className="ml-1">
+                                            {username}
+                                        </div>
+                                    </Col>
+                                    <Col xs={1}>
+                                        <FontAwesomeIcon icon={faEllipsisV}></FontAwesomeIcon>
+                                    </Col>
+                                </Row>
+                            </Card.Title>
+                            <Card.Img variant="top" src={image} className="fix-pictures"/>
+                            <Card.Body className="px-0 pt-1">
+                                <div>
+                                    <span className="px-1"><FontAwesomeIcon icon={faHeart}/></span>
+                                    <span className="px-1"><FontAwesomeIcon icon={faPaperPlane}/></span>
+                                    <span className="px-1"><FontAwesomeIcon icon={faComment}/></span>
+                                </div>
+                                <Card.Text>
+                                    {content}
+                                    <br/>
+                                    <small style={{color: 'grey'}}>5h</small>
+                                </Card.Text>
+                                <div className="decrease-fontsize">
+                                    <div className="pl-1" style={{fontWeight: 'bold'}}>Comments</div>
+                                    {
+                                        comments.map(comment => {
+                                            return <Comment username={comment.name}
+                                                            commentContent={comment.commentContent}></Comment>
+                                        })
+                                    }
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
 }
 
